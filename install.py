c1e032615ef34c546e4a556011ea393d5838ec60
范圣帅 <fanshengshuai@gmail.com>
2016-02-23 14:51:30 +0800

    修改安装文件

--- install.py
+++ install.py
@@ -14,13 +14,19 @@ import sys
 import platform
 import shlex
 import subprocess
-import urllib2
-import re
 import socket
+import getpass
 
 class Install(object):
 
     def __init__(self):
+
+        self.user = getpass.getuser()
+
+        if (self.user != 'root'):
+            print('Must run in root')
+            sys.exit()
+
         if hasattr(platform, 'linux_distribution'):
             self.dist = platform.linux_distribution(full_distribution_name=0)
         else:
@@ -30,13 +36,16 @@ class Install(object):
         self.installpath = '/usr/local/vpsmate'
         self.distname = self.dist[0].lower()
         self.version = self.dist[1]
-        
+        self.os = platform.system()
+
+
+
     def _run(self, cmd, shell=False):
         if shell:
             return subprocess.call(cmd, shell=shell)
         else:
             return subprocess.call(shlex.split(cmd))
-        
+
     def check_platform(self):
         supported = True
         if self.distname == 'centos':
@@ -45,6 +54,8 @@ class Install(object):
         elif self.distname == 'redhat':
             if float(self.version) < 5.4:
                 supported = False
+        elif self.os == 'Darwin':
+            supported = True
         #elif self.distname == 'ubuntu':
         #    if float(self.version) < 10.10:
         #        supported = False
@@ -54,7 +65,33 @@ class Install(object):
         else:
             supported = False
         return supported
-    
+
+    def check_git(self):
+        supported = True
+        try:
+            if self.distname == 'centos' or self.distname == 'redhat':
+                self._run("yum install -y git")
+        except:
+            pass
+
+        # if self.distname == 'centos':
+        #     if float(self.version) < 5.4:
+        #         supported = False
+        # elif self.distname == 'redhat':
+        #     if float(self.version) < 5.4:
+        #         supported = False
+        # elif self.os == 'Darwin':
+        #     supported = True
+        # #elif self.distname == 'ubuntu':
+        # #    if float(self.version) < 10.10:
+        # #        supported = False
+        # #elif self.distname == 'debian':
+        # #    if float(self.version) < 6.0:
+        # #        supported = False
+        # else:
+        #     supported = False
+        return supported
+
     def install_python(self):
         if self.distname in ('centos', 'redhat'):
             # following this: http://fedoraproject.org/wiki/EPEL/FAQ
@@ -82,24 +119,24 @@ class Install(object):
             pass
 
     def install_vpsmate(self):
-        localpkg_found = False
-        if os.path.exists(os.path.join(os.path.dirname(__file__), 'vpsmate.tar.gz')):
-            # local install package found
-            localpkg_found = True
-        else:
-            # or else install online
-            print '* Downloading install package from www.vpsmate.org'
-            f = urllib2.urlopen('http://www.vpsmate.org/api/latest')
-            data = f.read()
-            f.close()
-            downloadurl = re.search('"download":"([^"]+)"', data).group(1).replace('\/', '/')
-            self._run('wget -nv -c "%s" -O vpsmate.tar.gz' % downloadurl)
-            
-        # uncompress and install it
-        self._run('mkdir vpsmate')
-        self._run('tar zxmf vpsmate.tar.gz -C vpsmate')
-        if not localpkg_found: os.remove('vpsmate.tar.gz')
-        
+        # localpkg_found = False
+        # if os.path.exists(os.path.join(os.path.dirname(__file__), 'vpsmate.tar.gz')):
+        #     # local install package found
+        #     localpkg_found = True
+        # else:
+        #     # or else install online
+        #     print '* Downloading install package from www.vpsmate.org'
+        #     f = urllib2.urlopen('http://www.vpsmate.org/api/latest')
+        #     data = f.read()
+        #     f.close()
+        #     downloadurl = re.search('"download":"([^"]+)"', data).group(1).replace('\/', '/')
+        #     self._run('wget -nv -c "%s" -O vpsmate.tar.gz' % downloadurl)
+        #
+        # # uncompress and install it
+        # self._run('mkdir vpsmate')
+        # self._run('tar zxmf vpsmate.tar.gz -C vpsmate')
+        # if not localpkg_found: os.remove('vpsmate.tar.gz')
+
         # stop service
         print
         if os.path.exists('/etc/init.d/vpsmate'):
@@ -107,18 +144,20 @@ class Install(object):
 
         # backup data and remove old code
         if os.path.exists('%s/data/' % self.installpath):
-            self._run('cp -r %s/data/* vpsmate/data/' % self.installpath, True)
+            self._run('mkdir /tmp/vpsmate_data', True)
+            self._run('cp -r %s/data/* /tmp/vpsmate_data/' % self.installpath, True)
         self._run('rm -rf %s' % self.installpath)
-        
+        self._run('git clone https://github.com/fanshengshuai/VPSMate.git %s' % self.installpath)
+
         # install new code
-        self._run('mv vpsmate %s' % self.installpath)
+        # self._run('mv vpsmate %s' % self.installpath)
         self._run('chmod +x %s/config.py %s/server.py' % (self.installpath, self.installpath))
-        
+
         # install service
         initscript = '%s/tools/init.d/%s/vpsmate' % (self.installpath, self.distname)
         self._run('cp %s /etc/init.d/vpsmate' % initscript)
         self._run('chmod +x /etc/init.d/vpsmate')
-        
+
         # start service
         if self.distname in ('centos', 'redhat'):
             self._run('chkconfig vpsmate on')
@@ -131,7 +170,7 @@ class Install(object):
     def config(self, username, password):
         self._run('%s/config.py username "%s"' % (self.installpath, username))
         self._run('%s/config.py password "%s"' % (self.installpath, password))
-        
+
     def detect_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('www.baidu.com', 80))
@@ -143,7 +182,7 @@ class Install(object):
         # check platform environment
         print '* Checking platform...',
         supported = self.check_platform()
-        
+
         if not supported:
             print 'FAILED'
             print 'Unsupport platform %s %s %s' % self.dist
@@ -162,20 +201,38 @@ class Install(object):
             print '* Installing python 2.6 ...'
             self.install_python()
 
+        # check GIT version
+        print '* Checking GIT ...',
+        if self.check_git():
+            print 'OK'
+        else:
+            print 'FAILED'
+
+        # if sys.version_info[:2] == (2, 6):
+        #     print 'OK'
+        # else:
+        #     print 'FAILED'
+        #
+        #     # install the right version
+
+
+        #     print '* Installing python 2.6 ...'
+        #     self.install_python()
+
         # stop firewall
         if os.path.exists('/etc/init.d/iptables'):
             self._run('/etc/init.d/iptables stop')
-        
+
         # get the latest vpsmate version
         print '* Installing latest VPSMate'
         self.install_vpsmate()
-        
+
         # set username and password
         print
         print '============================'
         print '*    INSTALL COMPLETED!    *'
         print '============================'
-        print 
+        print
         username = raw_input('Admin username [default: admin]: ').strip()
         password = raw_input('Admin password [default: admin]: ').strip()
         if len(username) == 0:
@@ -183,13 +240,13 @@ class Install(object):
         if len(password) == 0:
             password = 'admin'
         self.config(username, password)
-        
+
         print
         print '* Username and password set successfully!'
-        print 
+        print
         print '* The URL of your VPSMate is:',
         print 'http://%s:8888/' % self.detect_ip()
-        print 
+        print
 
         pass
 
@@ -197,7 +254,7 @@ class Install(object):
 def main():
     install = Install()
     install.install()
-    
+
 
 if __name__ == "__main__":
     main()
